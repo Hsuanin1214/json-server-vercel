afaa05d40adec7961730f1ebe0b44c1b15e88efa
Hsuan <welcome09210801@gmail.com>
2023-10-28 21:15:44 +0800

    調整多一個"/"

--- auth/admin.js
+++ auth/admin.js
@@ -6,7 +6,7 @@ const addContent = document.querySelector(".addContent");
 //後臺取得資料
 // const tableBody = document.getElementById("tableBody");
 function tableData() {
-  axios.get("https://final-json-auth.onrender.com//views").then(function (response) {
+  axios.get("https://final-json-auth.onrender.com/views").then(function (response) {
     let result = response.data;
     console.log(result);
     const tableBody = document.getElementById("tableBody");
@@ -65,7 +65,7 @@ tableData();
 function addBtn() {
   if (addTitle.value !== "" || addContent.value !== "") {
     axios
-      .post(`https://final-json-auth.onrender.com//views`, {
+      .post(`https://final-json-auth.onrender.com/views`, {
         name: addTitle.value,
         description: addContent.value,
       })
@@ -87,7 +87,7 @@ let editTitle = document.querySelector('.editTitle');
   //編輯景點
   editId = location.href.split('=')[1];
   if(window.location.href == `https://hsuanin1214.github.io/test-fork-login/edit.html?id=${editId}`){
-    axios.get(`https://final-json-auth.onrender.com//views/${editId}`)
+    axios.get(`https://final-json-auth.onrender.com/views/${editId}`)
     .then(function (response) {
       console.log(response)
       editTitle.value = JSON.stringify(response.data.name).replace(/^"(.*)"$/, '$1');
@@ -102,7 +102,7 @@ function editCheckBtn(e){
   editId = location.href.split("=")[1];
   e.preventDefault();
   if (editTitle.value !== '' || editContent.value !== '') {
-      axios.patch(`https://final-json-auth.onrender.com//views/${editId}`, {
+      axios.patch(`https://final-json-auth.onrender.com/views/${editId}`, {
           "name": editTitle.value,
           "description": editContent.value
       })
@@ -125,7 +125,7 @@ function deleteBtn(e) {
   }
   let num = e.target.getAttribute("data-num");
   axios
-    .delete(`https://final-json-auth.onrender.com//views/${num}`)
+    .delete(`https://final-json-auth.onrender.com/views/${num}`)
     .then(function (response) {
       alert("已刪除!");
       tableData();
